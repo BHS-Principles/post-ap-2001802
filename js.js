0bb1634e0ba925e56fdbbfefd91736b6289e8588
Daniel Langlois <2001802@apps.nsd.org>
2025-05-20 17:30:17 +0000

    food for family

---
+++ js.js
@@ -0,0 +1,13 @@
+var LIST = [
+    "1",
+    "2",
+    "3",
+];
+
+var BTN = document.getElementById("myButton");
+
+for(var i = 0; i<3;i++){
+    var newBTN = BTN.cloneNode(true);
+    document.body.append(newBTN);
+    alert("Check me out" + LIST[i]);
+}
