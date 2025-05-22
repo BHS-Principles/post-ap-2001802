b6859e9e836c0e44f803ee0c09662007601b8253
Daniel Langlois <2001802@apps.nsd.org>
2025-05-22 16:55:50 +0000

    line 15

--- js.js
+++ js.js
@@ -8,6 +8,7 @@ var BTN = document.getElementById("myButton");
 
 for(var i = 0; i<3;i++){
     var newBTN = BTN.cloneNode(true);
+    newBTN.innerHTML =
     document.body.append(newBTN);
     alert("Check me out" + LIST[i]);
 }
