4e16fe7741413b8e29fd848bd58bbafd2078a645
Daniel Langlois <2001802@apps.nsd.org>
2025-05-30 17:38:05 +0000

    cards v3

--- js.js
+++ js.js
@@ -1,8 +1,79 @@
-draw = 2
-for(i=0;i<=draw;i++){
+var FRAMES = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+
+];
+
+var HTML = document.querySelector(".card");
+
+var doSomething = function(event, data){
+    console.log(event);
+    console.log(data);
+    alert("FRame me up before you go go");
+};
+
+for(var count = 0; count < FRAMES.length; count++ )
+{
+    var d = FRAMES[count];
+    var copy = HTML.cloneNode(true);
+    copy.style.backgroundPositionX = count%13 + "00%";
+    copy.style.backgroundPositionY = count%4 + "00%";
+    d.suit = Math.floor(count/13);
+    d.number = count%13;
+    copy.addEventListener("click", (e)=>{ doSomething(e,d) } );
+
+    document.body.append(copy);
 
 }
-/*
-formula, 100% = king for every additional 100% subtract one from card sheet ie... 200% = queen, 700% = 7
-*/
+alert("?????");
 
