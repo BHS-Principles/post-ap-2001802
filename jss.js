24b89c99ec04532d35b7f32684c82ef51778e0ed
Daniel Langlois <2001802@apps.nsd.org>
2025-06-02 17:30:26 +0000

    stable

--- jss.js
+++ jss.js
@@ -2,11 +2,10 @@ var TEMP = document.getElementById("temp");
 var TARGET = document.getElementById("target");
 var CARD = TEMP.querySelector(".card");
 
-for(var i=0;i<5;i++){
+for(var i=0;i<2;i++){
     var cardCopy = CARD.cloneNode(true);
-    cardCopy.innerHTML = "silver";
+    cardCopy.innerHTML = "";
     cardCopy.style.backgroundPositionX = i%13 + "00%";
     cardCopy.style.backgroundPositionY = i%4 + "00%";
     TARGET.append(cardCopy);
-
 }
