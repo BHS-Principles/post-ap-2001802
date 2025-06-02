d93367606e4ca37e665ca9cae439fbbda2a1d49a
Daniel Langlois <2001802@apps.nsd.org>
2025-06-02 18:06:34 +0000

    shuffle

--- jss.js
+++ jss.js
@@ -2,10 +2,23 @@ var TEMP = document.getElementById("temp");
 var TARGET = document.getElementById("target");
 var CARD = TEMP.querySelector(".card");
 
-for(var i=0;i<2;i++){
+var DECK = [
+
+];
+var cardCount = 52;
+for(var i=0;i<cardCount-2;i++){
     var cardCopy = CARD.cloneNode(true);
-    cardCopy.innerHTML = "";
-    cardCopy.style.backgroundPositionX = i%13 + "00%";
-    cardCopy.style.backgroundPositionY = i%4 + "00%";
+    cardCopy.innerHTML = "" + i;
+    cardCopy.style.backgroundPositionX = -i%13 + "00%";
+    cardCopy.style.backgroundPositionY = -i%4 + "00%";
     TARGET.append(cardCopy);
 }
+
+var shuffledDeck = function(x){
+    var cards = [];
+
+    Math.floor(Math.random()*x);
+    return cards;
+};
+
+DECK = shuffledDeck(x);
