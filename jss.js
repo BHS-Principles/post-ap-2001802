c8ec6b20a6767c8574d54d74793a53b87bcd9461
Daniel Langlois <2001802@apps.nsd.org>
2025-06-03 18:05:06 +0000

    cards5

--- jss.js
+++ jss.js
@@ -2,23 +2,60 @@ var TEMP = document.getElementById("temp");
 var TARGET = document.getElementById("target");
 var CARD = TEMP.querySelector(".card");
 
-var DECK = [
-
-];
+/*
 var cardCount = 52;
 for(var i=0;i<cardCount-2;i++){
     var cardCopy = CARD.cloneNode(true);
-    cardCopy.innerHTML = "" + i;
+    cardCopy.innerHTML = "" + (i+1);
     cardCopy.style.backgroundPositionX = -i%13 + "00%";
     cardCopy.style.backgroundPositionY = -i%4 + "00%";
     TARGET.append(cardCopy);
 }
+*/
 
-var shuffledDeck = function(x){
-    var cards = [];
-
-    Math.floor(Math.random()*x);
-    return cards;
+class Card{
+    constructor(num){
+        this.id = num;
+        this.suit = Math.floor(num / 13);
+        this.value = num % 13;
+        this.pic = "mine.svg";
+    }
+    draw(){
+        var cardCopy = CARD.cloneNode(true);
+        cardCopy.innerHTML = "" + (this.id);
+        cardCopy.style.backgroundPositionX = -this.id%13 + "00%";
+        cardCopy.style.backgroundPositionY = -this.id%4 + "00%";
+        TARGET.append(cardCopy);
+    }
 };
 
-DECK = shuffledDeck(x);
+var makeDeck = function(howMany){
+    var deck = [];
+
+    for(var i=0;i<howMany;i++){
+        var card = new Card(i);
+        deck.push(card);
+    }
+
+    return deck;
+};
+
+var shuffledDeck = function(deck){
+    for(var i=0;i<deck.length;i++){
+        var rnd = Math.floor(Math.random()*deck.length);
+
+        var card1 = deck[i];
+        var card2 = deck[rnd];
+        deck[rnd] = card1;
+        deck[i] = card2;
+        //cards need to switch positions
+
+    };
+    
+    return deck;
+};
+
+var DECK = makeDeck(52);
+shuffledDeck(DECK);
+console.log(DECK);
+DECK[0].draw();
