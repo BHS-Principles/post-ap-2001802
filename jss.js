907ff228adbe60f22fcfb4a858f185dded84b185
Daniel Langlois <2001802@apps.nsd.org>
2025-06-10 16:54:35 +0000

    maybe is work yes?

--- jss.js
+++ jss.js
@@ -2,11 +2,42 @@ var TEMP = document.getElementById("temp");
 var CARD = TEMP.querySelector(".card");
 var TARGET = document.getElementById("target");
 
+class Game{
+    constructor(players, deck){
+        this.players = players;
+        this.deck = deck;
+        this.activePlayer = players[0];
+        this.turn = 0;
+
+        this.play();
+       
+    }
+    notOver = function(){
+        alert("")
+        return this.turn++ < 5
+    }
+    play = function(){
+        //shuffle cards
+        this.deck.shuffle();
+
+        for(var i=0;i<this.players.length;i++){
+            this.deck.deal(this.players[i]);
+
+            while(this.notOver()){
+                alert(this.activePlayer.name + "is still playing");
+            };
+        };
+    };
+};
 
 class Player{
-    constructor(){
+    constructor(name){
+        this.name = name;
+        this.hand = [];
+    };
+    draw(){
 
-    }
+    };
 };
 
 class Deck{
@@ -28,9 +59,13 @@ class Deck{
     }
     shuffle(){
         for(var i=0;i<this.cardCount;i++){
-            i+= deck.Cards;
+            i+= Deck.Cards;
         }
     }
+    deal(player){
+        alert('I delt to: ' + player.name);
+    }
+    
 };
 
 class Card{
@@ -49,9 +84,14 @@ class Card{
     }
 };
 
-
-
+var plyz = [];
+plyz[0] = new Player("bill");
 var marieAntoinet = new Deck(52);
+
+
+var newgame = new Game(plyz, Deck);
+
+
 marieAntoinet.cards[0].draw();
 var williamOfOrange = new Card(25);
 williamOfOrange.draw();
